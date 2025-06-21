3fd4595980175ff28e3327635c5d25a950e04060
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2025-06-21 16:05:54 +0800

    remove some.md

--- TTS_API.py
+++ TTS_API.py
@@ -1,4 +1,4 @@
-TTS_URL = "127.0.0.1:9880"
+TTS_URL = "localhost:9880"
 
 #TTS static Setting
 referWavPath = "D:/vscode/GPT-SoVITS-beta1/Sample/proccessed/HaBE/感覺可以睡久一點我就睡了，然後醒來就已經下午兩點鐘了.wav";#temp data, this sould be setup in server not client
